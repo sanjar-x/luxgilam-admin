12864b4f9126eb83ed91a4d00545df7d714928f9
sanjar-x <sanjar68x@gmail.com>
2024-04-21 17:57:04 +0500

    salam

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-STATIC_URL = "static/"
+STATIC_URL = "app/staticfiles/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 STATIC_ROOT = os.path.join(BASE_DIR, "/app/staticfiles")
 
